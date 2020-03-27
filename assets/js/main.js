c9ce94160eab7d3b5dfc2f20e74cd184bfee99fe
m-olfatee <m.olfatee@gmail.com>
2020-03-27 10:30:10 +0100

    10.31

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,36 @@
 var listRow = 1
+var listDisplay = false
 function addList(category, x) {
+
     console.log(x)
     var obst = document.getElementById(category).value;
     var amount = document.getElementsByClassName("amount-number")[x].value;
-    var unit = document.getElementsByClassName("amount-unit")[x].value;
-    var displayList = document.getElementById("list-table");
-    var line = document.createElement("p");
-    line.innerHTML = listRow;
-    listRow++
-    var listItem = document.createElement("p");
-    listItem.innerHTML = ("Obst - " + obst);
-    var listAmount = document.createElement("p");
-    listAmount.innerHTML = amount;
-    var listUnit = document.createElement("p");
-    listUnit.innerHTML = unit;
-    displayList.appendChild(line);
-    displayList.appendChild(listItem);
-    displayList.appendChild(listAmount);
-    displayList.appendChild(listUnit);
+    if (amount == "") {
+        alert("Geben Sie bitte eine Menge an")
 
+    } else {
+        if (listDisplay === false) {
+            listDisplay = true;
+            document.getElementById("list").style.display = "block"
+        }
+        var unit = document.getElementsByClassName("amount-unit")[x].value;
+        var displayList = document.getElementById("list-table");
+        var line = document.createElement("p");
+        line.innerHTML = listRow;
+        listRow++
+        var listItem = document.createElement("p");
+        listItem.innerHTML = ("Obst - " + obst);
+        var listAmount = document.createElement("p");
+        listAmount.innerHTML = amount;
+        var listUnit = document.createElement("p");
+        listUnit.innerHTML = unit;
+        displayList.appendChild(line);
+        displayList.appendChild(listItem);
+        displayList.appendChild(listAmount);
+        displayList.appendChild(listUnit);
+    }
+}
+function imageChanger(category, y) {
+    var z = document.getElementById(category).value;
+    var image = document.getElementsByClassName("image")[y].src = `assets/img/${z}_icon.png`;
 }
